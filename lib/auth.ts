42344189258212f18ed39f3ce0d08456032c85bc
Guilherme Dantas <guilhermedantas788@gmail.com>
2023-03-16 14:22:28 -0300

    auth lib and nextjs config bcrypt

---
+++ lib/auth.ts
@@ -0,0 +1,5 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = (password) => bcrypt.hash(password, 10)
+
+const comparePasswords = (plainTextPassword, hashedPassword) => bcrypt.compare(plainTextPassword, hashedPassword)
